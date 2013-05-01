d9684a583a931b28da9f89162326f6a93a5cafd2
Rasarts <rasart.pro@gmail.com>
2013-05-01 12:54:45 +0400

    deploy

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,7 +5,7 @@ class SectionsController < ApplicationController
   # GET /sections
   # GET /sections.json
   def index
-    @sections = Section.includes(:categories).order('created_at DESC').all
+    @sections = Section.order(:created_at).includes(:categories).order('created_at DESC').all
 
     respond_to do |format|
       format.html # index.html.erb
